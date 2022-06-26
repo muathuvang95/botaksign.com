ac05a8a305efc715274f56549ac9af3349d946a9
muathuvang95 <45276880+muathuvang95@users.noreply.github.com>
2022-06-26 21:49:17 +0700

    Update dashboard my account

---
+++ themes/printcart-child/woocommerce/myaccount/form-change-password.php
@@ -0,0 +1,38 @@
+<?php
+/**
+ * Change password
+ *
+ */
+
+defined( 'ABSPATH' ) || exit;
+
+do_action( 'woocommerce_before_edit_account_form' ); ?>
+
+<form class="woocommerce-EditAccountForm edit-account" action="" method="post" <?php do_action( 'woocommerce_edit_account_form_tag' ); ?> >
+
+	<fieldset>
+		<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
+			<label for="password_current"><?php esc_html_e( 'Current password', 'woocommerce' ); ?></label>
+			<input type="password" class="woocommerce-Input woocommerce-Input--password input-text" name="password_current" id="password_current" autocomplete="off" />
+		</p>
+		<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
+			<label for="password_1"><?php esc_html_e( 'New password', 'woocommerce' ); ?></label>
+			<input type="password" class="woocommerce-Input woocommerce-Input--password input-text" name="password_1" id="password_1" autocomplete="off" />
+		</p>
+		<p class="woocommerce-form-row woocommerce-form-row--wide form-row form-row-wide">
+			<label for="password_2"><?php esc_html_e( 'Confirm new password', 'woocommerce' ); ?></label>
+			<input type="password" class="woocommerce-Input woocommerce-Input--password input-text" name="password_2" id="password_2" autocomplete="off" />
+		</p>
+	</fieldset>
+	<div class="clear"></div>
+
+	<p class="nb-submit-change">
+		<?php wp_nonce_field( 'nb_save_account_password', 'save-account-change-password-nonce' ); ?>
+		<button type="submit" class="woocommerce-Button button" name="nb_save_account_password" value="<?php esc_attr_e( 'Save changes', 'woocommerce' ); ?>"><?php esc_html_e( 'Save changes', 'woocommerce' ); ?></button>
+		<input type="hidden" name="action" value="nb_save_account_password" />
+	</p>
+
+	<?php do_action( 'woocommerce_edit_account_form_end' ); ?>
+</form>
+
+<?php do_action( 'woocommerce_after_edit_account_form' ); ?>
