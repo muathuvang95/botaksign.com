7496b0277022e14748721de5e49bd52a06abde9e
muathuvang95 <45276880+muathuvang95@users.noreply.github.com>
2022-10-05 17:37:28 +0700

    Update test.php

--- themes/printcart-child/test.php
+++ themes/printcart-child/test.php
@@ -18,13 +18,13 @@ function nb_coppy_folder_from_s3($uri, $new_name = '') {
                 'secret' => $awsSecretKey
             )
         ]);
-        if( substr($uri, -1) != '/') {
-            $uri .= '/';
-        }
+
+        $uri = trim($uri, '/'). '/';
 
         $objects = $s3->getIterator('ListObjects', array('Bucket' => $bucket, 'Prefix' => $uri, 'Delimiter'=>'/'));
         foreach ($objects as $key => $object) {
             $path = $object['Key'];
+
             $ext = pathinfo($path, PATHINFO_EXTENSION);
             $basename = basename($path);
             if($ext) {
@@ -32,21 +32,18 @@ function nb_coppy_folder_from_s3($uri, $new_name = '') {
                 if( count($uri_array) > 1 ) {
                     $uri_array[count($uri_array) - 2] = $new_name;
                     $path_new = implode('/' , $uri_array);
-                } else {
-                    $path_new = $new_name;
+                    $res = $s3->copyObject([
+                        'Bucket'     => $bucket,
+                        'Key'        => "{$path_new}{$basename}",
+                        'CopySource' => "{$bucket}/{$path}",
+                    ]);
                 }
-
-                $res = $s3->copyObject([
-                    'Bucket'     => $bucket,
-                    'Key'        => "{$path_new}{$basename}",
-                    'CopySource' => "{$bucket}/{$path}",
-                ]);
             }
             
         }
     }
 }
 
-nb_coppy_folder_from_s3('reupload-design-test/1625f101618194348_preview', '123123');
+nb_coppy_folder_from_s3('/reupload-design/01896861660756693/', 'muathuvang12313aw');
 
 ?>
