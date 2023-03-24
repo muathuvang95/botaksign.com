71cc7d6ecba5e6ceb73e114d455879eaf37dede3
Vo Tuan Vu <45276880+muathuvang95@users.noreply.github.com>
2023-03-25 01:42:23 +0700

    Update style quantity in cart

--- themes/printcart-child/woocommerce/cart/cart.php
+++ themes/printcart-child/woocommerce/cart/cart.php
@@ -204,7 +204,7 @@ function render_product_and_service($cart_item_key, $cart_item, $_product, $prod
                 <div class="col-lg-4 col-6 btk-product-quantity mb-1" data-title="<?php esc_attr_e('Quantity', 'woocommerce'); ?>">
                     <?php
                     if ($_product->is_sold_individually()) {
-                        $product_quantity = sprintf('1 <input type="hidden" name="cart[%s][qty]" value="1" />', $cart_item_key);
+                        $product_quantity = sprintf('<input type="hidden" name="cart[%s][qty]" value="1" />', $cart_item_key);
                     } else {
                         $product_quantity = woocommerce_quantity_input(
                             array(
@@ -221,7 +221,7 @@ function render_product_and_service($cart_item_key, $cart_item, $_product, $prod
                     ?>
                 </div>
                 <div class="col-lg-8 col-6 mb-1">
-                    <div class="btk-product-edit">
+                    <div class="btk-product-edit btk-btn-light mb-1">
                         <?php
                             $link = add_query_arg(
                                 array(
@@ -232,12 +232,12 @@ function render_product_and_service($cart_item_key, $cart_item, $_product, $prod
                             $edit_html = '<a href="%s" class="remove" aria-label="%s" data-product_id="%s" data-product_sku="%s">'.$svg_edit.' <span class="btk-title-action">Edit</span></a>';
                             echo apply_filters(// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
                                 'woocommerce_cart_item_edit_link', sprintf(
-                                    $edit_html, $link, esc_html__('Remove this item', 'woocommerce'), esc_attr($product_id), esc_attr($_product->get_sku())
+                                    $edit_html, $link, esc_html__('Edit this item', 'woocommerce'), esc_attr($product_id), esc_attr($_product->get_sku())
                                 ), $cart_item_key
                             );
                         ?>
                     </div>
-                    <div class="btk-product-remove mr-1 mb-1">
+                    <div class="btk-product-remove btk-btn-light mr-1 mb-1">
                         <?php
                         $svg_trash = '<svg xmlns:dc="http://purl.org/dc/elements/1.1/" xmlns:cc="http://creativecommons.org/ns#" xmlns:rdf="http://www.w3.org/1999/02/22-rdf-syntax-ns#" xmlns:svg="http://www.w3.org/2000/svg" xmlns="http://www.w3.org/2000/svg" xmlns:sodipodi="http://sodipodi.sourceforge.net/DTD/sodipodi-0.dtd" xmlns:inkscape="http://www.inkscape.org/namespaces/inkscape" viewBox="0 -256 1792 1792" id="svg3741" version="1.1" inkscape:version="0.48.3.1 r9886" width="20" height="20" sodipodi:docname="trash_font_awesome.svg"> <g transform="matrix(1,0,0,-1,197.42373,1255.0508)" id="g3743"> <path d="M 512,800 V 224 q 0,-14 -9,-23 -9,-9 -23,-9 h -64 q -14,0 -23,9 -9,9 -9,23 v 576 q 0,14 9,23 9,9 23,9 h 64 q 14,0 23,-9 9,-9 9,-23 z m 256,0 V 224 q 0,-14 -9,-23 -9,-9 -23,-9 h -64 q -14,0 -23,9 -9,9 -9,23 v 576 q 0,14 9,23 9,9 23,9 h 64 q 14,0 23,-9 9,-9 9,-23 z m 256,0 V 224 q 0,-14 -9,-23 -9,-9 -23,-9 h -64 q -14,0 -23,9 -9,9 -9,23 v 576 q 0,14 9,23 9,9 23,9 h 64 q 14,0 23,-9 9,-9 9,-23 z M 1152,76 v 948 H 256 V 76 Q 256,54 263,35.5 270,17 277.5,8.5 285,0 288,0 h 832 q 3,0 10.5,8.5 7.5,8.5 14.5,27 7,18.5 7,40.5 z M 480,1152 h 448 l -48,117 q -7,9 -17,11 H 546 q -10,-2 -17,-11 z m 928,-32 v -64 q 0,-14 -9,-23 -9,-9 -23,-9 h -96 V 76 q 0,-83 -47,-143.5 -47,-60.5 -113,-60.5 H 288 q -66,0 -113,58.5 Q 128,-11 128,72 v 952 H 32 q -14,0 -23,9 -9,9 -9,23 v 64 q 0,14 9,23 9,9 23,9 h 309 l 70,167 q 15,37 54,63 39,26 79,26 h 320 q 40,0 79,-26 39,-26 54,-63 l 70,-167 h 309 q 14,0 23,-9 9,-9 9,-23 z" id="path3745" inkscape:connector-curvature="0" style="fill:currentColor"/> </g></svg>';
                         $remove_html = '<a href="%s" class="remove" aria-label="%s" data-product_id="%s" data-product_sku="%s">'.$svg_trash.' <span class="btk-title-action">Remove</span></a>';
@@ -248,7 +248,7 @@ function render_product_and_service($cart_item_key, $cart_item, $_product, $prod
                         );
                         ?>
                     </div>
-                    <div class="btk-product-remove mb-1" data-toggle="collapse" data-target=".btk-collapse-<?php echo esc_attr($cart_item_key); ?>" aria-expanded="false" aria-controls="btk-collapse-<?php echo esc_attr($cart_item_key); ?>-left btk-collapse-<?php echo esc_attr($cart_item_key); ?>-right">
+                    <div class="btk-product-remove btk-btn-light mb-1" data-toggle="collapse" data-target=".btk-collapse-<?php echo esc_attr($cart_item_key); ?>" aria-expanded="false" aria-controls="btk-collapse-<?php echo esc_attr($cart_item_key); ?>-left btk-collapse-<?php echo esc_attr($cart_item_key); ?>-right">
                         <div class="btk-target-show-more-item btk-button-down active" >
                             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="currentColor" class="bi bi-chevron-down" viewBox="0 0 16 16">
                                 <path fill-rule="evenodd" d="M1.646 4.646a.5.5 0 0 1 .708 0L8 10.293l5.646-5.647a.5.5 0 0 1 .708.708l-6 6a.5.5 0 0 1-.708 0l-6-6a.5.5 0 0 1 0-.708z"/>
--- themes/printcart-child/woocommerce/global/quantity-input.php
+++ themes/printcart-child/woocommerce/global/quantity-input.php
@@ -28,9 +28,10 @@ global $product;
 //     $min_value = $f_product->get_height_stock_amount();
 // }
 if(empty($max_value)){ $max_value = 9999; }
-$input_style = printcart_get_options('nbcore_add_cart_style');
+// $input_style = printcart_get_options('nbcore_add_cart_style');
+$input_style = 'style-2';
 ?>
-	<div class="nb-quantity <?php echo esc_attr($input_style); ?>">
+	<div class="nb-quantity input-group">
 		<?php if('style-1' == $input_style): ?>
 			<input type="number" class="input-text qty text" step="<?php echo esc_attr( $step ); ?>" min="<?php echo esc_attr( $min_value ); ?>" max="<?php echo esc_attr( 0 < $max_value ? $max_value : '' ); ?>" name="<?php echo esc_attr( $input_name ); ?>" value="<?php echo esc_attr( $input_value ); ?>" title="<?php echo esc_attr_x( 'Qty', 'Product quantity input tooltip', 'printcart' ) ?>" size="4" pattern="<?php echo esc_attr( $pattern ); ?>" inputmode="<?php echo esc_attr( $inputmode ); ?>" />
 			<div class="qty-buttons">
@@ -38,8 +39,12 @@ $input_style = printcart_get_options('nbcore_add_cart_style');
 				<span class="wac-btn-sub quantity-minus-cart pt-icon-minus"></span>
 			</div>
 		<?php else: ?>
-			<span class="wac-btn-sub quantity-minus pt-icon-minus"></span>
-			<input type="number" class="input-text qty text" step="<?php echo esc_attr( $step ); ?>" min="<?php echo esc_attr( $min_value ); ?>" max="<?php echo esc_attr( 0 < $max_value ? $max_value : '' ); ?>" name="<?php echo esc_attr( $input_name ); ?>" value="<?php echo esc_attr( $input_value ); ?>" title="<?php echo esc_attr_x( 'Qty', 'Product quantity input tooltip', 'printcart' ) ?>" size="4" pattern="<?php echo esc_attr( $pattern ); ?>" inputmode="<?php echo esc_attr( $inputmode ); ?>" />
-			<span class="wac-btn-inc quantity-plus-cart pt-icon-plus"></span>
+			<div class="input-group-prepend">
+				<span class="wac-btn-sub btn btn-outline-light quantity-minus-cart pt-icon-minus qty-button btk-btn-light"></span>
+			</div>
+			<input type="number" class="input-text qty text form-control" step="<?php echo esc_attr( $step ); ?>" min="<?php echo esc_attr( $min_value ); ?>" max="<?php echo esc_attr( 0 < $max_value ? $max_value : '' ); ?>" name="<?php echo esc_attr( $input_name ); ?>" value="<?php echo esc_attr( $input_value ); ?>" title="<?php echo esc_attr_x( 'Qty', 'Product quantity input tooltip', 'printcart' ) ?>" size="4" pattern="<?php echo esc_attr( $pattern ); ?>" inputmode="<?php echo esc_attr( $inputmode ); ?>" />
+			<div class="input-group-append">
+				<span class="wac-btn-inc btn btn-outline-light quantity-plus-cart pt-icon-plus qty-button btk-btn-light"></span>
+			</div>
 		<?php endif; ?>
 	</div>
